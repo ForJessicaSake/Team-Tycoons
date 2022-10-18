f94fd8668ebc6d445e3cb80bbb2f98d6d7c5f3a5
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-10-18 21:20:49 +0100

    navbar

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ const navslide = () => {
     const burger = document.querySelector(".hamburger-icon");
     const links = document.querySelector(".links");
     const navLinks = document.querySelectorAll(".links li");
+    const unactive = document.querySelector(".unactive");
+
 
     burger.addEventListener("click", () => {
-        //Toggle hamburger icon
+        //Toggle burger
         links.classList.toggle("nav-active");
-
         //Animate links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = ''
+                link.style.animation = "";
             } else {
-                link.style.animation = `navLinksfade 0.5s ease forwards ${index / 5 + 0.5}s`;
+                link.style.animation = `navLinksfade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
     });
 };
+
 navslide();
+
+
+// // Javascript for Hero/Nav section assigned to Juliet
+// const overlayMenu = document.querySelector(".links");
+// const toggleMenu = () => {
+//   hamburger-icon.classList.toggle("nav-active");
+// };
