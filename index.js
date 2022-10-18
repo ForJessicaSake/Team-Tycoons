f0de99259da40104561ca908c8ee5bc884326286
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-10-18 09:37:52 +0100

    initial push

---
+++ index.js
@@ -0,0 +1,20 @@
+const navslide = () => {
+    const burger = document.querySelector(".hamburger-icon");
+    const links = document.querySelector(".links");
+    const navLinks = document.querySelectorAll(".links li");
+
+    burger.addEventListener("click", () => {
+        //Toggle hamburger icon
+        links.classList.toggle("nav-active");
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinksfade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+        });
+    });
+};
+navslide();
